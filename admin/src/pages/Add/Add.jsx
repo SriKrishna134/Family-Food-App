a783ee8f1c0b7eda9e5f221547b5ebf58ba42fdf
Sri Krishna <145562895+SriKrishna134@users.noreply.github.com>
2024-09-16 03:53:41 +0530

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -3,7 +3,7 @@ import './Add.css'
 import { assets } from '../../assets/assets'
 import axios from "axios"
 
-const Add = ({url}) => { 
+const Add = () => { 
     
     const [image, setImage] = useState(false);
     const [data, setData] = useState({
@@ -27,7 +27,7 @@ const Add = ({url}) => {
         formData.append("description", data.description)
         formData.append("category", data.category)
         formData.append("image", image )
-        const response = await axios.post(`${url}/api/food/add`, formData)
+        const response = await axios.post(`https://family-food-app-backend.onrender.com//api/food/add`, formData)
         if(response.data.success){
             setData({
                 name: '',
