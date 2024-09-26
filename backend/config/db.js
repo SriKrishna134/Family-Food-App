bc57f6ed986a401baa1759a446e9b488bd2d3fe3
Srikrishna Hireholi <145562895+SriKrishna134@users.noreply.github.com>
2024-09-26 23:20:14 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 // connect  db //
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+SERVER').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://srikrishna:9019755311@cluster0.oyzyh.mongodb.net/Big_food_App').then(()=>console.log("DB Connected"));
 }
