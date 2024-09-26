9b92ae14c0f821467d4289a3957cb570897246e7
Srikrishna Hireholi <145562895+SriKrishna134@users.noreply.github.com>
2024-09-26 23:35:27 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -48,8 +48,9 @@ const placeOrder = async (req, res) => {
                 const session = await stripe.checkout.sessions.create({
                     line_items: line_items,
                     mode: 'payment',
-                    success_url: `₹{frontendurl}/verify?success=true&orderId=${newOrder._id}`,
-                    cancel_url: `₹{frontendurl}/verify?success=false&orderId=${newOrder._id}`,
+                    success_url: `₹{frontendurl}/verify?success=true&orderId=₹{newOrder._id}`,
+                    cancel_url: `₹{frontendurl}/verify?success=false&orderId=₹{newOrder._id}`,
+
                 });
 
                 res.json({ success: true, session_url: session.url });
